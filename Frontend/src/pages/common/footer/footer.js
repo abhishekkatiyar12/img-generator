58642b027ead7b3a765bde4d0ccfb76ac7a549fa
Abhishek Katiyar <abhishek.katiyar.983932@gmail.com>
2024-06-08 19:07:10 +0530

    second

---
+++ Frontend/src/pages/common/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="homepage-footer">
+                <p>Contact us: abhishekatiyarr@gmail.com</p>
+                <div className="social-media-links">
+                    <a href="#facebook">Facebook</a>
+                    <a href="#twitter">Twitter</a>
+                    <a href="#instagram">Instagram</a>
+                </div>
+            </footer>
+  );
+};
+
+export default Footer;
